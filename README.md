819a49e084ce54be7ebe7f4359c86810314a824c
Will <will@willj.co>
2022-09-25 19:25:08 -0500

    Prototype

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
+<<<<<<< HEAD
 # HappinessPlatform
 A platform for measuring the end user's happiness
+=======
+# Happiness Platform
+
+The Happiness Platform is a chat application that helps you keep track of your happiness.
+
+The questions asked are based on the [Happiness Inventory](https://archive.ics.uci.edu/ml/datasets/Sentiment+Labelled+Sentences#). This dev environment simulates five happiness questions that you can run machine learning models against.
+
+## Happiness Questions
+* At work or school this week, what did you notice around you?
+* What's an activity you're interested in right now and why do you like it?
+* Tell me about a friend you connected with this week?
+* Tell me about an activity you did that you're proud of this week?
+* What are your plans for next week?
+
+## Machine Learning
+
+The admin panel provides input boxes for API endpoints and their respective keys. Each time a message is retrieved, it will execute a call against the provided endpoint with the user's response to the latest question.
+
+### Training Dataset
+
+While you could use any labelled set of sentences, an easy to use one is [Sentiment Labelled Sentences](https://archive.ics.uci.edu/ml/datasets/Sentiment+Labelled+Sentences#) to train a custom sentiment analysis model.
+
+>>>>>>> 6d1ae5f (Prototype)
--- fetch.js
+++ fetch.js
@@ -1,6 +1,7 @@
 let questionPosition = -1;
 let currentResponses = {}
 
+// UI
 function clearChat(){
    console.log("Attempting to clear the chat");
    var chatElements = document.getElementsByClassName("message-pair");
@@ -8,6 +9,14 @@ function clearChat(){
    for (let i = (chatElements.length-1); i >= 0; i--) {
       chatElements[i].remove();
     }
+}
+
+function makeConsoleElement(consoleMessage){
+   var ranAt = Date(Date.now())
+   var consoleParagraph = document.createElement('p');
+      consoleParagraph.className = 'console-message';
+      consoleParagraph.innerHTML = ranAt.toString() + ": " + consoleMessage;
+   return consoleParagraph;
 }
 
 function makeChatElement(botMessage, userMessage){
@@ -25,6 +34,10 @@ function makeChatElement(botMessage, userMessage){
    
    return section;
 }
+
+function insertConsole(node){}
+
+// MAIN
 
 async function getMessage(){
    console.log("Getting New Message");
@@ -68,8 +81,7 @@ async function getMessage(){
    
 }
 
-
-
+// Get Session Data from Server
 async function getSession(sessionId="0001") {
    var localSessionId = sessionId;
    if (localSessionId === ""){
@@ -94,3 +106,70 @@ async function getSession(sessionId="0001") {
          console.log(err.message);
       });
 } 
+// Call REST APIs
+function callAzureML(endpoint, key){
+
+}
+
+async function callAzureCogSvc(endpoint, key, message){
+   let cogSvcsResponse = {};
+   await fetch(endpoint,
+      {
+         method: 'POST',
+         headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Ocp-Apim-Subscription-Key': key,
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(message)
+      },
+   )
+      .then((response) => response.json())
+      .then((data) => {
+         console.log(data);
+         cogSvcsResponse = data;
+      })
+      .catch((err) => {
+         console.log(err.message);
+      });
+   return cogSvcsResponse;
+}
+
+function callCogSvc(){
+   console.log("Calling a Cognitive Service for Sentiment");
+   var resp = currentResponses["response"][questionPosition][1];
+   
+   // Get the first task's endpoint and key
+   var task1Endpoint = document.getElementById("task1endpoint").value;
+   var task1Key = document.getElementById("task1key").value;
+   if (task1Endpoint === "" || task1Key === ""){
+      console.error("Task 1 Endpoint or Key is not defined");
+      return ;
+   }
+
+   payload = {
+      documents:[
+         {
+            id:"01",
+            text:resp
+         }
+      ]
+   }
+   
+   callAzureCogSvc(
+      task1Endpoint,
+      task1Key,
+      payload
+   ).then((thing) =>{
+      console.log("Cog Services Response:");
+      console.log(thing);
+      //Get console
+      var consoleBody = document.getElementById("console");
+      var elem = makeConsoleElement(JSON.stringify(thing, null, 5));
+      consoleBody.insertBefore(elem, consoleBody.firstChild);
+   }).catch((err) => {
+      console.log("Cog Service Error:")
+      console.log(err);
+   });
+   
+}
