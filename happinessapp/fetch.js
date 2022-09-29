cc6495e8a8d06a90a0069e0b14987c8fc646b110
Will <will@willj.co>
2022-09-28 22:02:00 -0500

    Adding s task 2 and task 3

--- happinessapp/fetch.js
+++ happinessapp/fetch.js
@@ -2,97 +2,97 @@ let questionPosition = -1;
 let currentResponses = {}
 
 // UI
-function clearChat(){
+function clearChat() {
    console.log("Attempting to clear the chat");
    var chatElements = document.getElementsByClassName("message-pair");
-   console.log("There are "+chatElements.length+" message pair elements");
-   for (let i = (chatElements.length-1); i >= 0; i--) {
+   console.log("There are " + chatElements.length + " message pair elements");
+   for (let i = (chatElements.length - 1); i >= 0; i--) {
       chatElements[i].remove();
-    }
+   }
 }
 
-function makeConsoleElement(consoleMessage, isError=false){
+function makeConsoleElement(consoleMessage, isError = false) {
    var ranAt = Date(Date.now())
    var consoleParagraph = document.createElement('p');
-      consoleParagraph.className = 'console-message';
-      consoleParagraph.innerHTML = ranAt.toString() + ": " + consoleMessage;
-   if (isError){
+   consoleParagraph.className = 'console-message';
+   consoleParagraph.innerHTML = ranAt.toString() + ": " + consoleMessage;
+   if (isError) {
       consoleParagraph.classList.add('console-error');
    }
    return consoleParagraph;
 }
 
-function makeChatElement(botMessage, userMessage){
+function makeChatElement(botMessage, userMessage) {
    var botParagraph = document.createElement('p');
-       botParagraph.className = 'bot';
-       botParagraph.innerHTML = botMessage;
+   botParagraph.className = 'bot';
+   botParagraph.innerHTML = botMessage;
    var userParagraph = document.createElement('p');
-       userParagraph.className = 'user';
-       userParagraph.innerHTML = userMessage;
-   
+   userParagraph.className = 'user';
+   userParagraph.innerHTML = userMessage;
+
    var section = document.createElement('section');
-       section.className = 'message-pair';
-       section.appendChild(botParagraph)
-       section.appendChild(userParagraph)
-   
+   section.className = 'message-pair';
+   section.appendChild(botParagraph)
+   section.appendChild(userParagraph)
+
    return section;
 }
 
-function insertConsole(node){}
+function insertConsole(node) { }
 
 // MAIN
 
-async function getMessage(){
+async function getMessage() {
    console.log("Getting New Message");
    var startingIdFormInput = document.getElementById("startfrom")
    var sessionId = startingIdFormInput.value;
    questionPosition += 1;
 
-   if ("next" in currentResponses){
-      console.log("Updating next with "+currentResponses.next);
+   if ("next" in currentResponses) {
+      console.log("Updating next with " + currentResponses.next);
       sessionId = currentResponses.next;
    }
 
-   if (sessionId === "" || Number(questionPosition) > 4){
+   if (sessionId === "" || Number(questionPosition) > 4) {
       console.log("Requesting New Session");
       await getSession(sessionId);
       questionPosition = 0;
       startingIdFormInput.value = sessionId;
 
-      if ("next" in currentResponses){
-         console.log("Updating next with "+currentResponses.next);
+      if ("next" in currentResponses) {
+         console.log("Updating next with " + currentResponses.next);
          sessionId = currentResponses.next;
          startingIdFormInput.innerHTML = sessionId;
       }
 
-      
+
       // Clear messages
       clearChat();
       // Clear console
    }
 
-   
+
    // Display message
    var chatElement = document.getElementById("chat")
    var resp = currentResponses["response"][questionPosition]
 
    var messageElement = makeChatElement(resp[0], resp[1])
-   
+
    chatElement.insertBefore(messageElement, chatElement.firstChild);
    // Call task API
    // Display console
-   
+
 }
 
 // Get Session Data from Server
-async function getSession(sessionId="0001") {
+async function getSession(sessionId = "0001") {
    var localSessionId = sessionId;
-   if (localSessionId === ""){
+   if (localSessionId === "") {
       console.log("getSession: Using Default session id");
       localSessionId = "0001";
    }
 
-   await fetch('http://127.0.0.1:5000/session/'+localSessionId,
+   await fetch('http://127.0.0.1:5000/session/' + localSessionId,
       {
          method: 'GET',
          headers: {
@@ -108,13 +108,13 @@ async function getSession(sessionId="0001") {
       .catch((err) => {
          console.log(err.message);
       });
-} 
+}
 // Call REST APIs
-function callAzureML(endpoint, key){
+function callAzureML(endpoint, key) {
 
 }
 
-async function callAzureCogSvc(endpoint, key, message){
+async function callAzureCogSvc(endpoint, key, message) {
    let cogSvcsResponse = {};
    let rawResponse = null;
    await fetch(endpoint,
@@ -132,7 +132,7 @@ async function callAzureCogSvc(endpoint, key, message){
       .then((cogSvcResponse) => {
          var consoleBody = document.getElementById("console");
          let isCogSvcsError = false;
-         if ("error" in cogSvcResponse){
+         if ("error" in cogSvcResponse) {
             isCogSvcsError = true;
          }
          var elem = makeConsoleElement(JSON.stringify(cogSvcResponse), isCogSvcsError);
@@ -144,40 +144,138 @@ async function callAzureCogSvc(endpoint, key, message){
    return cogSvcsResponse;
 }
 
-function task1ApiCall(){
+async function callAzureMlSvc(endpoint, key, deployment, message) {
+   let azMlResponse = {};
+   let rawResponse = null;
+   await fetch(endpoint,
+      {
+         method: 'POST',
+         headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': 'Bearer '+key,
+            'Content-Type': 'application/json',
+            'azureml-model-deployment': deployment
+         },
+         body: JSON.stringify(message)
+      },
+   )
+      .then((response) => response.json())
+      .then((amlResponse) => {
+         var consoleBody = document.getElementById("console");
+         let isAmlError = false;
+         if ("error" in amlResponse) {
+            isAmlError = true;
+         }
+         var elem = makeConsoleElement(JSON.stringify(amlResponse), isAmlError);
+         azMlResponse = amlResponse;
+         consoleBody.insertBefore(elem, consoleBody.firstChild);
+      })
+      .catch((err) => {
+         console.log(err.message);
+      });
+   return azMlResponse;
+}
+
+function task3ApiCall(){
    console.log("Calling a Cognitive Service for Sentiment");
    var resp = currentResponses["response"][questionPosition][1];
-   
+
    // Get the first task's endpoint and key
-   var task1Endpoint = document.getElementById("task1endpoint").value;
-   var task1Key = document.getElementById("task1key").value;
-   if (task1Endpoint === "" || task1Key === ""){
-      console.error("Task 1 Endpoint or Key is not defined");
-      return ;
+   var task3Endpoint = document.getElementById("task3endpoint").value;
+   var task3Deployment = document.getElementById("task3Deployment").value;
+   var task3Key = document.getElementById("task3key").value;
+   if (task3Endpoint === "" || task3Deployment === "" || task3Key === "") {
+      console.error("Task 3 Endpoint or Key is not defined");
+      return;
    }
 
    payload = {
-      documents:[
-         {
-            id:"01",
-            text:resp
-         }
+      input_data: [
+         [
+            resp
+         ]
       ]
    }
-   
-   callAzureCogSvc(
-      task1Endpoint,
-      task1Key,
+
+   callAzureMlSvc(
+      task3Endpoint,
+      task3Key,
+      task3Deployment,
       payload
-   ).then((cogSvcResponse) =>{
+   ).then((amlResponse) => {
       console.log("Successful call for task 1 API");
    }).catch((err) => {
       console.log("Cog Service Error:")
       console.log(err);
    });
-   
 }
 
-function task2ApiCall(){
 
+function task1ApiCall() {
+   console.log("Calling a Cognitive Service for Sentiment");
+   var resp = currentResponses["response"][questionPosition][1];
+
+   // Get the first task's endpoint and key
+   var task1Endpoint = document.getElementById("task1endpoint").value;
+   var task1Key = document.getElementById("task1key").value;
+   if (task1Endpoint === "" || task1Key === "") {
+      console.error("Task 1 Endpoint or Key is not defined");
+      return;
+   }
+
+   payload = {
+      documents: [
+         {
+            id: "01",
+            text: resp
+         }
+      ]
+   }
+
+   callAzureCogSvc(
+      task1Endpoint,
+      task1Key,
+      payload
+   ).then((cogSvcResponse) => {
+      console.log("Successful call for task 1 API");
+   }).catch((err) => {
+      console.log("Cog Service Error:")
+      console.log(err);
+   });
+}
+
+function task2ApiCall() {
+   var resp = currentResponses["response"][questionPosition][1];
+
+   // Get the first task's endpoint and key
+   var task2Endpoint = document.getElementById("task2endpoint").value;
+   var task2Key = document.getElementById("task2key").value;
+   if (task2Endpoint === "" || task2Key === "") {
+      console.error("Task 2 Endpoint or Key is not defined");
+      return;
+   }
+
+   payload = {
+      kind: "EntityRecognition",
+      analysisInput: {
+         documents: [
+            {
+               id: "01",
+               text: resp,
+               language: "en"
+            }
+         ]
+      }
+   }
+
+   callAzureCogSvc(
+      task2Endpoint,
+      task2Key,
+      payload
+   ).then((cogSvcResponse) => {
+      console.log("Successful call for task 2 API");
+   }).catch((err) => {
+      console.log("Cog Service Error:")
+      console.log(err);
+   });
 }
